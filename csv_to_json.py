6af1029a7456f2d1504f1576656dab1cb0e14758
moritzcm <moritz.cm@gmail.com>
2017-04-21 09:55:20 +0200

    update csv_to_json

--- csv_to_json.py
+++ csv_to_json.py
@@ -4,7 +4,7 @@ import json
 columns = ['timestamp','probes', 'nSites','nQueries','nResponses','q25RTT','q50RTT','q75RTT','q90RTT']
 df = pd.read_csv('./data/k-during-root-dns-nov2015-events.csv', header=None, names=columns)
 
-df['timestamp'] = df['timestamp']*1000
+df['timestamp'] = df['timestamp']*100
 
 df_probes = df[['timestamp', 'probes']]
 df_median_rtt = df[['timestamp', 'q50RTT']]
