d430aec568e1ace927dc01d135848a0a2fa08000
moritzcm <moritz.cm@gmail.com>
2017-04-21 10:33:31 +0200

    new data

--- csv_to_json.py
+++ csv_to_json.py
@@ -9,4 +9,8 @@ df['timestamp'] = df['timestamp']*1000
 df_probes = df[['timestamp', 'probes']]
 df_median_rtt = df[['timestamp', 'q50RTT']]
 
-print(df_probes.to_json(orient='values'))
+f = open('./data/reachability.json', 'w')
+f.write(df_probes.to_json(orient='values'))
+
+f = open('./data/rtt.json', 'w')
+f.write(df_median_rtt.to_json(orient='values'))
